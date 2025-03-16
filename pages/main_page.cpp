b066ee843787e033762102eea2b03c30996c8dd2
Arief Satria <128447209+illufoxKusanagi@users.noreply.github.com>
2025-03-16 17:00:47 +0700

    style: slicing main page design (not fixed)

--- pages/main_page.cpp
+++ pages/main_page.cpp
@@ -2,27 +2,14 @@
 
 MainPage::MainPage(QWidget *parent)
     : QWidget(parent), mainLayout(new QVBoxLayout(this)) {
-  mainLayout->setContentsMargins(0, 0, 0, 0);
-  mainLayout->setSpacing(0);
-  QWidget *dragWidget = new QWidget(this);
-  QVBoxLayout *dragLayout = new QVBoxLayout(dragWidget);
-  dragLayout->setContentsMargins(0, 0, 0, 0);
-  dragWidget->setStyleSheet("background-color: " + Colors::Secondary100.name() +
-                            ";"
-                            "border: 2px dashed " +
-                            Colors::Secondary800.name() + ";");
-  QLabel *dragLabel = new QLabel("Seret dan lepas gambar di sini", this);
-  dragLabel->setAlignment(Qt::AlignCenter);
-  QPushButton *browseButton = new QPushButton("Pilih Gambar", this);
-  connect(browseButton, &QPushButton::clicked, this,
-          &MainPage::setupImageInput);
-  dragLayout->addWidget(dragLabel);
+  mainLayout->setContentsMargins(16, 16, 16, 16);
+  mainLayout->setSpacing(32);
 
-  InputType inputType = InputType("field", "contoh");
-  InputWidget *inputWidget = new InputWidget(inputType, this);
-  mainLayout->addWidget(dragWidget);
+  ButtonAction *browseButton = new ButtonAction("Pilih Gambar", "upload", this);
+  browseButton->setFixedSize(150, 50);
+  browseButton->setEnabled(true);
+  setupImageLayout();
   mainLayout->addWidget(browseButton);
-  mainLayout->addWidget(inputWidget);
   setLayout(mainLayout);
   // setStyleSheet("QWidget {"
   //               "    background-color: " +
@@ -31,4 +18,45 @@ MainPage::MainPage(QWidget *parent)
   //               "}");
 }
 
-void MainPage::setupImageInput() { qDebug() << "Button clicked"; }
+void MainPage::setupImageInput() {
+  QWidget *dragWidget = new QWidget(this);
+  QVBoxLayout *dragLayout = new QVBoxLayout(dragWidget);
+  dragLayout->setContentsMargins(0, 0, 0, 0);
+  dragWidget->setStyleSheet("background-color: " + Colors::Secondary100.name() +
+                            ";"
+                            "border: 2px dashed " +
+                            Colors::Secondary900.name() + ";");
+  QLabel *dragIcon = new QLabel(this);
+  QLabel *dragLabel = new QLabel("Drop your image here...", this);
+  QPixmap pixmap(":/icons/icons/upload.svg");
+  QPixmap scaledPixmap = pixmap.scaled(40, 40, Qt::KeepAspectRatio);
+  // dragIcon->setFixedSize(80, 80);
+  dragIcon->setPixmap(scaledPixmap);
+  dragIcon->setAlignment(Qt::AlignCenter);
+  dragLabel->setAlignment(Qt::AlignCenter);
+  dragLayout->addWidget(dragIcon);
+  dragLayout->addWidget(dragLabel);
+  m_imageLayout->addWidget(dragWidget);
+}
+
+void MainPage::setupExtensionButton() {
+  QWidget *buttonWidget = new QWidget(this);
+  QVBoxLayout *buttonLayout = new QVBoxLayout(buttonWidget);
+  buttonLayout->setContentsMargins(0, 0, 0, 0);
+  buttonLayout->setSpacing(16);
+  buttonWidget->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
+  InputWidget *sourceExtension =
+      new InputWidget(InputType("dropdown", "From"), this);
+  InputWidget *targetExtension =
+      new InputWidget(InputType("dropdown", "To"), this);
+  buttonLayout->addWidget(sourceExtension);
+  buttonLayout->addWidget(targetExtension);
+  m_imageLayout->addWidget(buttonWidget);
+}
+
+void MainPage::setupImageLayout() {
+  m_imageLayout = new QHBoxLayout(this);
+  setupImageInput();
+  setupExtensionButton();
+  mainLayout->addLayout(m_imageLayout);
+}
