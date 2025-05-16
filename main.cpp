298743f000ac5c5780ff84cd79a44186efe048ad
Arief Satria <128447209+illufoxKusanagi@users.noreply.github.com>
2025-05-16 14:40:30 +0700

    style: setting new app icon

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
+  QCoreApplication::setOrganizationName("Illufox Kasunagi");
+  QCoreApplication::setApplicationName("Image Converter");
+  QIcon appIcon;
+  appIcon.addFile(":/icons/icons/icon-logo.png", QSize(64, 64));
+  app.setWindowIcon(appIcon);
   app.setStyleSheet("QWidget { background-color: white; color: black;}");
   MainWindow w;
   w.show();
